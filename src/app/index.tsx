3533db4d00d0b6554c554d59a201a4fdd6973593
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2024-02-06 16:15:33 -0300

    feat: select items

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,29 @@
 import CategoryButton from "@/components/categoryButtton";
 import { CATEGORIES } from "@/utils/data/products";
+import { useState } from "react";
 import { FlatList, View } from "react-native";
 import Header from "../components/header";
 
 function Home() {
+  const [category, setCategory] = useState(CATEGORIES[0]);
+
+  function handleCategoryChange(newCategory: string) {
+    setCategory(newCategory);
+  }
+
   return (
     <View className="flex-1">
       <Header title="Faça o seu pedido" cartQuantity={2} />
       <FlatList
         data={CATEGORIES}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <CategoryButton title={item} />}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            onPress={() => handleCategoryChange(item)}
+            isSelected={category === item}
+          />
+        )}
         horizontal
         className="max-h-10 mt-5"
         contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
