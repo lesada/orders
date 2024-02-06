03e659f8e08678e793a7722b857a9b2320eb8745
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2024-02-06 15:21:21 -0300

    feat: fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,10 +4,36 @@ import {
   useSafeAreaInsets,
 } from "react-native-safe-area-context";
 
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  useFonts,
+} from "@expo-google-fonts/inter";
+import { useEffect } from "react";
+
 SplashScreen.preventAutoHideAsync();
 
 function Layout() {
   const insets = useSafeAreaInsets();
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
     <SafeAreaProvider
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: "Inter_600SemiBold",
+        subtitle: "Inter_500Medium",
+        body: "Inter_400Regular",
+        bold: "Inter_700Bold",
+      },
+    },
   },
   plugins: [],
 };
